c73faf972b338a27b2d583a57efd3a84544783fd
Edward Dale <scompt@scompt.com>
2021-12-02 06:11:11 +0100

    Day 02, Part 2

--- day_02/__main__.py
+++ day_02/__main__.py
@@ -3,17 +3,19 @@ from typing import TextIO, List, Tuple
 def read_lines(textio: TextIO) -> List[Tuple[int]]:
     depth = 0
     horiz = 0
+    aim = 0
     lines = textio.readlines()
     for line in [l.strip() for l in lines]:
         axis, distance = line.split(' ', 1)
         distance = int(distance)
         print(axis, distance)
         if axis == 'up':
-            depth -= distance
+            aim -= distance
         elif axis == 'down':
-            depth += distance
+            aim += distance
         elif axis == 'forward':
             horiz += distance
+            depth += aim * distance
         print(depth, horiz, depth * horiz)
 
 if __name__ == "__main__":
