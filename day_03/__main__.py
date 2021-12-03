01f8140e7d0a45868b97f1ed0a8d926c2006c7f8
Edward Dale <scompt@scompt.com>
2021-12-03 07:22:45 +0100

    A bit of a cleanup

--- day_03/__main__.py
+++ day_03/__main__.py
@@ -1,7 +1,7 @@
 from utils import read_lines
 import sys
 
-BIT_COUNT=12
+BIT_COUNT=int(sys.argv[1])
 
 def count_bit_position(numbers, bit):
     count = 0
@@ -10,61 +10,46 @@ def count_bit_position(numbers, bit):
             count += 1
     return count
 
-def asdf(numbers):
+def calculate_power_consumption(numbers):
     gamma = 0
     epsilon = 0
-    omega = 0
     line_count = len(numbers)
     for bit in range(BIT_COUNT)[::-1]:
-        one_count = 0
-        for number in numbers:
-            if number & (1<<bit):
-                one_count += 1
-        if one_count > line_count/2:
+        one_count = count_bit_position(numbers, bit)
+        zeroes_count = line_count - one_count
+        if one_count >= zeroes_count:
             gamma += (1<<bit)
-        elif one_count == line_count / 2:
-            omega += (1<<bit)
 
     for bit in range(BIT_COUNT):
         if gamma & (1<<bit) == 0:
             epsilon += (1<<bit)
-    return gamma, epsilon, omega
+    return gamma, epsilon, gamma*epsilon
 
-def qwer(numbers):
-    life_support_rating = 1
-    gamma, epsilon, omega = asdf(numbers)
-    print(bin(omega))
+def calculate_life_support_rating(numbers):
     o2_candidates = list(numbers)
     co2_candidates = list(numbers)
     for bit in range(BIT_COUNT)[::-1]:
-        print(bit, [bin(c) for c in o2_candidates])
-        ones = count_bit_position(o2_candidates, bit)
-        zeroes = len(o2_candidates) - ones
-        print(ones, zeroes)
-        if ones >= zeroes:
-            o2_candidates = [cand for cand in o2_candidates if cand & (1<<bit)]
-        elif ones < zeroes:
-            o2_candidates = [cand for cand in o2_candidates if not cand & (1<<bit)]
-        if len(o2_candidates) == 1:
-            life_support_rating = life_support_rating * o2_candidates[0]
-            print('o2', o2_candidates[0])
-        
-        ones = count_bit_position(co2_candidates, bit)
-        zeroes = len(co2_candidates) - ones
-        print(ones, zeroes)
-        if ones < zeroes:
-            co2_candidates = [cand for cand in co2_candidates if cand & (1<<bit)]
-        elif ones >= zeroes:
-            co2_candidates = [cand for cand in co2_candidates if not cand & (1<<bit)]
-        if len(co2_candidates) == 1:
-            life_support_rating = life_support_rating * co2_candidates[0]
-            print('co2', co2_candidates[0])
-        print(life_support_rating)
+        if len(o2_candidates) > 1:
+            ones = count_bit_position(o2_candidates, bit)
+            zeroes = len(o2_candidates) - ones
+            if ones >= zeroes:
+                o2_candidates = [cand for cand in o2_candidates if cand & (1<<bit)]
+            elif ones < zeroes:
+                o2_candidates = [cand for cand in o2_candidates if not cand & (1<<bit)]
 
+        if len(co2_candidates) > 1:
+            ones = count_bit_position(co2_candidates, bit)
+            zeroes = len(co2_candidates) - ones
+            if ones < zeroes:
+                co2_candidates = [cand for cand in co2_candidates if cand & (1<<bit)]
+            elif ones >= zeroes:
+                co2_candidates = [cand for cand in co2_candidates if not cand & (1<<bit)]
+    return o2_candidates[0], co2_candidates[0], o2_candidates[0] * co2_candidates[0]
 
 
 spec = {"bits": str}
 numbers = [int(line['bits'], 2) for line in read_lines(sys.stdin, spec)]
-gamma, epsilon, omega = asdf(numbers)
-# print(gamma, epsilon, gamma * epsilon)
-qwer(numbers)
+_, _, power_consumption = calculate_power_consumption(numbers)
+print(power_consumption)
+_, _, life_support_rating = calculate_life_support_rating(numbers)
+print(life_support_rating)
