c8d990ffb8c8d2395a525564d58ef800b14cfd58
Edward Dale <scompt@scompt.com>
2021-12-20 13:43:28 +0100

    Day 20, Part 2

--- day_20/__main__.py
+++ day_20/__main__.py
@@ -35,6 +35,15 @@ class Image:
     def __len__(self):
         return len(self.points)
 
+    def cutout(self, from_point: Point, to_point: Point):
+        out = Image(self.algo)
+        for y in range(from_point.y, to_point.y):
+            for x in range(from_point.x, to_point.x):
+                point = Point(x=x,y=y)
+                out.set(point, point in self.points)
+        return out
+
+
     def __str__(self):
         out = ''
         for y in range(self.min_y, self.max_y+1):
@@ -54,7 +63,7 @@ class Image:
 
 
     def step(self) -> "Image":
-        extend = 10
+        extend = 0
         new_image = Image(self.algo)
         for y in range(self.min_y-extend, self.max_y+extend+1):
             for x in range(self.min_x-extend, self.max_x+extend+1):
@@ -76,37 +85,36 @@ def read_input(textio: TextIO):
             image.set(Point(x,y), char == '#')
     return image
 
+
 image = read_input(sys.stdin)
-
-
+extend = 100
+image.min_x -= extend
+image.min_y -= extend
+image.max_x += extend
+image.max_y += extend
+orig_from = Point(x=image.min_x-2, y=image.min_y-2)
+orig_to = Point(x=image.max_x+3, y=image.max_y+3)
+mid_from = Point(x=image.min_x-20, y=image.min_y-20)
+mid_to = Point(x=image.max_x+20, y=image.max_y+20)
 print(image)
 print()
-image = image.step()
-print(image)
-print()
-image = image.step()
-print(image)
-print()
 # image = image.step()
 # print(image)
 # print()
 # image = image.step()
 # print(image)
 # print()
-# image = image.step()
+# image = image.cutout(orig_from, orig_to)
 # print(image)
 # print()
 # image = image.step()
 # print(image)
 # print()
-# image = image.step()
-# print(image)
 # print()
-# image = image.step()
-# print(image)
-# print()
-# image = image.step()
-# print(image)
-# print()
-# image = image.step()
+for i in range(50):
+    # if i % 2 == 0:
+    #     image = image.cutout(mid_from, mid_to)
+    image = image.step()
+    print(image)
+# image = image.cutout(orig_from, orig_to)
 # print(image)
