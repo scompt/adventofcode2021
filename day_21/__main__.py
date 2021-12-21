9795885d8778ff75efb85a9720451a4ffe0a72cc
Edward Dale <scompt@scompt.com>
2021-12-21 13:04:01 +0100

    Day 21, Part 2

--- day_21/__main__.py
+++ day_21/__main__.py
@@ -1,27 +1,59 @@
-from typing import TextIO
+from collections import defaultdict
+from typing import TextIO, List, Generator, Tuple
 import sys
 import itertools
+from functools import cache
+from dataclasses import dataclass
+
+TARGET_SCORE = 21
+NUM_POSITIONS = 10
+ROLLS_PER_TURN = 3
+DIE_SIZE=3
+
+ROLLS = [sum(rolls) for rolls in list(itertools.product(range(1,DIE_SIZE+1), repeat=ROLLS_PER_TURN))]
+
+@dataclass(frozen=True, eq=True)
+class GameState:
+    positions: Tuple[int]
+    scores: Tuple[int]=(0,0)
+    player_turn: int=0
+
+    def is_winner(self) -> bool:
+        return any(score>=TARGET_SCORE for score in self.scores)
+    
+    def get_winner(self) -> int:
+        winning_score = next(filter(lambda score: score >= TARGET_SCORE, self.scores))
+        return self.scores.index(winning_score)
+    
+    def next_turns(self) -> Generator["GameState", None, None]:
+        next_player_turn = (self.player_turn+1)%2
+        for roll in ROLLS:
+            next_scores = list(self.scores)
+            next_positions = list(self.positions)
+            next_positions[self.player_turn] = (next_positions[self.player_turn] - 1 + roll) % NUM_POSITIONS + 1
+            next_scores[self.player_turn] += next_positions[self.player_turn]
+            yield GameState(positions=tuple(next_positions), scores=tuple(next_scores), player_turn=next_player_turn)
 
 def read_input(textio:TextIO):
     lines = textio.readlines()
     player_1 = int(lines[0][-2:-1])
     player_2 = int(lines[1][-2:-1])
-    return [player_1, player_2]
+    return GameState(positions=(player_1, player_2))
 
-die = itertools.cycle(range(1,100+1))
+@cache
+def traverse(state) -> Tuple[int, int]:
+    winners = [0, 0]
+    if state.is_winner():
+        winners[state.get_winner()] += 1
 
-positions = read_input(sys.stdin)
-scores = [0,0]
-turns = itertools.cycle(range(2))
-
-for turn_num, player_num in enumerate(turns):
-    positions[player_num] = (positions[player_num] + sum(next(die) for _ in range(3))) % 10
-    if positions[player_num] == 0:
-        scores[player_num] += 10
     else:
-        scores[player_num] += positions[player_num]
-    # print(player_num, positions[player_num], scores[player_num])
-    if scores[player_num] >= 1000:
-        break
+        for child in state.next_turns():
+            child_winners = traverse(child)
+            winners[0] += child_winners[0]
+            winners[1] += child_winners[1]
 
-print(min(scores) * (turn_num+1)*3)
+    return tuple(winners)
+
+state = read_input(sys.stdin)
+winners = traverse(state)
+print(max(winners))
